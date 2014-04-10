857a5eee1771e2e9d5978681ae42acf2854f80d0
maartenkohlmann <maartenkohlmann@gmail.com>
2014-04-10 14:03:55 +0200

    removed project

--- src/main/java/com/is/buyabike/repository/IdeaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.is.buyabike.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.is.buyabike.dto.IdeaDto;
-
-public interface IdeaRepository  extends JpaRepository<IdeaDto, Integer> {
-
-}
--- src/main/java/com/is/buyabike/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.is.buyabike.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.is.buyabike.model.Product;
-
-public interface ProductRepository  extends JpaRepository<Product, Integer> {
-
-}
