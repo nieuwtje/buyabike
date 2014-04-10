4e08ae849e750cb6435adf0ea8adac00f4265c02
maartenkohlmann <maartenkohlmann@gmail.com>
2014-04-10 12:49:06 +0200

    added test

---
+++ src/test/java/com/is/buyabike/controller/ClientControllerTest.java
@@ -0,0 +1,14 @@
+package com.is.buyabike.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientControllerTest {
+
+	@Test
+	public void testLogin() {
+		fail("Not yet implemented");
+	}
+
+}
