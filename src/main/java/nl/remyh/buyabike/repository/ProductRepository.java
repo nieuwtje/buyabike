e664e253718dcf70b5761b1111d12aa20794a2b7
r <e1971799@drdrb.com>
2014-04-10 09:59:46 +0200

    removed remy package

--- src/main/java/nl/remyh/buyabike/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package nl.remyh.buyabike.repository;
-
-import nl.remyh.buyabike.dto.ProductDto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductDto, Integer> {
-
-}
