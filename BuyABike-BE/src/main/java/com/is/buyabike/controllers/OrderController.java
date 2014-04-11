07c1c326bd9fcf8f21b99f6aeadf214bea9bfa56
nieuwtje <t.nieuwenhuys@hotmail.com>
2014-04-11 12:10:10 +0200

    headers: accept=application/json
    accept=application/xml doens't work for some reason

--- BuyABike-BE/src/main/java/com/is/buyabike/controllers/OrderController.java
+++ BuyABike-BE/src/main/java/com/is/buyabike/controllers/OrderController.java
@@ -1,5 +1,6 @@
 package com.is.buyabike.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.is.buyabike.domain.Product;
 import com.is.buyabike.domain.order.Order;
+import com.is.buyabike.domain.order.OrderItem;
 import com.is.buyabike.services.OrderService;
 
 @Controller
@@ -23,8 +26,29 @@ public class OrderController {
 		service.create(order);
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody List<Order> testOrder() {
+	// TODO - werkt niet???
+	@RequestMapping(method = RequestMethod.GET, headers = "accept=application/xml")
+	public @ResponseBody List<Order> getOrdersAsXML() {
+//		final List<Order> list = new ArrayList<Order>();
+//		Order order1 = new Order();
+//		Product p1 = new Product("test", "test product", "url.jpg", 10.50, 10.50, 5);
+//		Product p2 = new Product("test2", "test product2", "url.jpg", 12.50, 12.50, 20);
+//		Product p3 = new Product("test3", "test product3", "url.jpg", 8.50, 8.50, 15);
+//		OrderItem item1 = new OrderItem(p1, 5);
+//		order1.addOrderItem(item1);
+//		OrderItem item2 = new OrderItem(p2, 2);
+//		order1.addOrderItem(item2);
+//		OrderItem item3 = new OrderItem(p3, 3);
+//		order1.addOrderItem(item3);
+//		list.add(order1);
+//		
+//		return list;
+		
 		return service.listOrders();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, headers = "accept=application/json")
+	public @ResponseBody List<Order> getOrdersAsJSON() {
+		return getOrdersAsXML();
 	}
 }
